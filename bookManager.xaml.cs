fc1f442a54c7750bcf942bc44b67f6eaeb2df3b4
NateTheGrappler <guzinskinatan@gmail.com>
2025-12-15 19:38:33 -0500

    UPDATE: Added in Front end Ui for Book Manager

---
+++ bookManager.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for bookManager.xaml
+    /// </summary>
+    public partial class bookManager : Page
+    {
+        public bookManager()
+        {
+            InitializeComponent();
+        }
+
+
+        //--------------------------------------Front UI------------------------------------------
+        
+        //This function is for main backbutton thats in the title to go back to the neural network
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+    }
+}
