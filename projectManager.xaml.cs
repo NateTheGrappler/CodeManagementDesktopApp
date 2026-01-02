935c21517fd9dd542303356b06c42ceb89df3bbc
NateTheGrappler <guzinskinatan@gmail.com>
2026-01-02 15:16:04 -0500

    Initial Commit

---
+++ projectManager.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for projectManager.xaml
+    /// </summary>
+    public partial class projectManager : Page
+    {
+        public projectManager()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+    }
+}
